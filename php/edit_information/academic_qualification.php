9eeece26989b870abef4b34d148cfee789062ed8
zoexploits <oladipupoa50@gmail.com>
2021-09-22 13:59:33 +0100

    first page assigned to me done

---
+++ php/edit_information/academic_qualification.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>apcv_maker</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+<div id="root">
+    <!-- this is the drawer for the apcv maker site -->
+    <?php
+    include_once "../components/drawer.php";
+    echo drawer($domain, "edit");
+    ?>
+    <?php
+    /* TODO form container for the apcv site
+     *  write all your code inside the div with id of form-container
+     */
+    ?>
+    <div id="form-container">
+        <?php
+        /*
+        * Todo Make sure to use any style you want for the form labels and input tags
+        *  but make sure to save your style in the css directory and make sure not to override
+        *  the style.css which is our base style incharge of the fonts and the drawer style
+        *  thank you
+        */
+
+        ?>
+        <h1>Academic Qualifications (With Dates and Grantings)</h1>
+        <h5>Please kindly fill the institutions attended. (Added new Qualification)</h5>
+        <br>
+        <h2>Qualification</h2>
+        <select>
+            <option >Select...</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+        <br>
+        <h2>Award Institution</h2>
+        <select>
+            <option >Select...</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+        <br>
+        <h2>Granting Date</h2>
+        <select>
+            <option >Day</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+        <select>
+            <option >Month</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+        <select>
+            <option >Year</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+        <br>
+        <button>save</button>
+        <button>Add New Qualification</button>
+
+
+    </div>
+</div>
+</body>
+</html>
