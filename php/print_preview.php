604b64e1565222286bde63ca12fed8b058555374
deeptones <eleshinnlataiwo08@gmail.com>
2021-09-29 15:18:06 +0100

    print_preview

--- php/print_preview.php
+++ php/print_preview.php
@@ -26,7 +26,248 @@
         *  thank you
         */
         ?>
-        <h1> Apcv sturff</h1>
+        <div class="form-container">
+        <img src="../static/img/College_logo_new.png" alt="college_logo" style="height: 72px; width: 72px; border-radius: 50%;">
+        <div>
+            <h1 style="font-weight: 400; font-size: 36px; text-align: center; line-height: 46.8px;">2017 PROMOTION EXCERCISE<br/>CURRICULUM VITAE</h1>
+        </div>
+       <div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">I</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Basic Information</h1></div>
+<div style="padding-left: 38px; line-height: 23px; float: left; padding-right: 285.05px;">
+    <div style="font-size: 12px;">(a) Name:<br/>
+    (b) Date of Birth:<br/>
+    (c) Department:<br/>
+    (d) Faculty:<br/></div>
+</div>
+
+<div style="padding-left: 38px; line-height: 23px; width: 1000px;">
+    <div style="font-size: 12px;">Adetoun Opeyemi Adediran<br/>
+    24 October, 1985<br/>
+    Nursing<br/>
+    Clinical Sciences<br/></div>
+</div>
+
+<div class="II" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">II</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Academic Position Information</h1></div>
+<div style="padding-left: 38px; line-height: 23px; float: left; padding-right: 200px;">
+    <div style="font-size: 12px;">(a) First Academic Appointment:<br/>
+    (b) Present Post:<br/>
+    (c) Date Of Birth:<br/>
+    (d) Department:<br/>
+    (e) Faculty:<br/></div>
+</div>
+
+<div style="padding-left: 38px; line-height: 23px; width: 1000px;">
+    <div style="font-size: 12px;">Lecturer 1<br/>
+    Lecturer 2<br/>
+    24 October, 1985<br/>
+    Nursing<br/>
+    Clinical Sciences<br/></div>
+</div>
+</div>
+
+
+
+<div class="III" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">III</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>University Education</h1></div>
+<div style="padding-left: 38px; line-height: 23px; float: left; padding-right: 267.234px;">
+    <div style="font-size: 12px;">University of Ibadan<br/>
+    </div>
+</div>
+
+<div style="padding-left: 38px; line-height: 23px; width: 1000px;">
+    <div style="font-size: 12px;">1990 - 1992<br/>
+   </div>
+</div>
+</div>
+
+
+
+<div class="IV" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">IV</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Academic Qualifications</h1></div>
+<div style="padding-left: 38px; line-height: 23px; float: left; padding-right: 137.384px;">
+    <div style="font-size: 12px;">B.Sc.(Agr.) (Bachelors Degree in Agriculture)<br/>
+    University of Ibadan<br>
+    B.Sc.(Agr.) (Bachelors Degree in Astronomy)<br/>
+    University of Ibadan
+    </div>
+</div>
+
+<div style="padding-left: 38px; line-height: 23px; width: 1000px;">
+    <div style="font-size: 12px;">2001<br/>
+    <br/>
+    2002<br/>
+   </div>
+</div>
+</div>
+
+<div class="V" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">V</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Professional Qualifications and Diplomas</h1></div>
+<div style="padding-left: 38px; line-height: 23px; float: left; padding-right: 142.868px;">
+    <div style="font-size: 12px;">B.Sc.(Agr.) Bachelors Degree in Agricultural<br/>
+    Economics 2005
+    </div>
+</div>
+
+<div style="padding-left: 38px; line-height: 23px; width: 1000px;">
+    <div style="font-size: 12px;">National Postgraduate College of Physicians (Fam.Med.)<br/>
+   </div>
+</div>
+</div>
+
+<div class="VI" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">VI</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Scholarship, Fellowships and Prizes With respect to Undergraduate Postgraduate work</h1></div>
+<div style="padding-left: 38px; line-height: 23px; float: left; padding-right: 267.234px;">
+    <div style="font-size: 12px;">Best Graduating Student
+    </div>
+</div>
+</div>
+
+
+
+<div class="VII" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">VII</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Honours, Distinctions and Membership of Learned Society</h1></div>
+<div style="padding-left: 38px; line-height: 23px; float: left; padding-right: 267.234px;">
+    <div style="font-size: 12px;">...<br/>
+
+    </div>
+</div>
+
+
+<div class="IX" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">IX</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Research</h1></div>
+<div style="padding-left: 38px; line-height: 23px; padding-right: 267.234px;">
+    <div style="font-size: 15px;">Completed<br/>
+    <div style="font-size: 12px; padding-left: 20px;">Best Graduating Student<br/></div>
+
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">In Progress<br/>
+    <div style="font-size: 12px; padding-left: 20px;">Best Graduating Student<br/></div>
+
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">Project, Dissertation and Thesis<br/>
+    <div style="font-size: 12px; padding-left: 20px;">Best Graduating Student<br/></div>
+
+    </div>
+</div>
+</div>
+
+<div class="X" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">X</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Publications</h1></div>
+<div style="padding-left: 38px; line-height: 23px; padding-right: 267.234px;">
+    <div style="font-size: 15px;">Books already published:  Nil<br/>
+    
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">Chapters in books already published<br/>
+    
+
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">Articles that have appeared in Refereed Conference Proceeding: Nil<br/>
+    
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">Patents and Copyrights: Nil<br/>
+    
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">Articles that have already appeared in learned journals:<br/>
+    <div style="font-size: 12px; padding-left: 20px;">Adediran, A.O and Adebiyi, A.A. (2010). Anatomy of Humans. Acta Pediatrica<br/></div>
+    <div style="font-size: 12px; padding-left: 20px;">Vol.2.No.10:1-10pp (Sweden) (Contribution: 50%)<br/></div>
+
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">Books, Chapters in Books and Articles already accepted for Publication: Nil<br/>
+    
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">Technical Reports and Monographs: Nil<br/>
+    
+    </div>
+
+    <div style="font-size: 15px; padding-top: 20px;">Chapters in Edited Books/ Revised Chapters in Edited Books: Nil<br/>
+    
+    </div>
+</div>
+</div>
+
+<div class="XI" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">XI</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Major Conferences Attended with Papers Read (in the last 5 years): Nil</h1></div>
+<div style="padding-left: 38px; line-height: 23px; float: left; padding-right: 267.234px;">
+    <div style="font-size: 12px;">...<br/>
+
+    </div>
+</div>
+
+<div class="VI" style="padding-top: 25px;">
+<div> <h1 style="float: left; padding-right: 38px; font-size: 24px; font-weight: 400; width: 10px;">XII</h1></div>
+<div style="padding-bottom: 15px; font-weight: 500; font-size: 24px; line-height: 31px; text-decoration: underline solid black 2px; color: #000000;
+"><h1>Ten Best Publications that Reflect the Totality of my Contributions to Scholarship:</h1></div>
+<div style="padding-left: 38px; line-height: 23px; padding-right: 267.234px;">
+<div style="font-size: 12px; padding-left: 20px;">Adediran, A.O and Adebiyi, A.A. (2010). Anatomy of Humans. Acta Pediatrica<br/></div>
+    <div style="font-size: 12px; padding-left: 20px;">Vol.2.No.10:1-10pp (Sweden) (Contribution: 50%)<br/></div>
+    <div style="font-size: 15px; padding-top: 20px;">Research Focus: Nil<br/>
+    
+    </div>
+</div>
+</div>
+
+<div style="padding-top: 60px; padding-left: 38px; float: left;">
+<div style="border: 1px solid black; width: 235px;"></div>
+<p style="text-align: center; width: 235px; padding-top: 15px;">Names</p><br/>
+
+</div>
+
+
+<div style="padding-top: 60px; padding-left: 38px; float: left;">
+<div style="border: 1px solid black; width: 235px;"></div>
+<p style="text-align: center; width: 235px; padding-top: 15px;">Signature</p><br/>
+</div>
+
+
+<div style="padding-top: 60px; padding-left: 38px; float: left; padding-bottom: 50px;">
+<div style="border: 1px solid black; width: 235px;"></div>
+<p style="text-align: center; width: 235px; padding-top: 15px;">Date</p><br/>
+</div><br/>
+
+
+<div style="width: 819px; text-align: center;">
+                <input type="submit" name="submit" value="Print"
+                       style="font-weight: 400; font-size: 25px; line-height: 29.3px; color: #f8f9fa; background: #212529; border-radius: 5px; height: 37px; width: 180px; border: none;">
+            </div>
+
+
+
+
+
+
+
+
+
+
+</div>
     </div>
 </div>
 <script rel="script" src="../js/drawer.js"></script>
